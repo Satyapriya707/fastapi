8513f3f6f796655baa7a57afb5d446cd6b80ac6f
Satyapriya707 <67006226+Satyapriya707@users.noreply.github.com>
2021-08-23 15:16:58 +0530

    Delete app directory

--- app/model/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class listOfAllFiles(BaseModel):
-    directoryPath: str
-    listOfFiles: List[str]
-
-class CountOfFiles(BaseModel):
-    numberOfFiles: int
